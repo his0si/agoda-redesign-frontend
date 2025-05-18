f12659b062e760f22dd2d78e254f50c59df4cced
HAE2NI <hae0_02ni@ewhain.net>
2025-05-18 18:32:18 +0900

    [ design ] upper components

---
+++ agoda_toy/src/stInfo/components/Upper/Upper.tsx
@@ -0,0 +1,15 @@
+import Search from './Search';
+import Photo from './Photo';
+import Description from './Description/Description';
+import Pop from './pop/Pop';
+
+export default function Upper() {
+  return (
+    <div>
+      <Search />
+      <Photo />
+      <Description />
+      <Pop />
+    </div>
+  );
+}
