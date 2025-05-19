3a642db692c2eb62bf14484ae4248b9627e6853d
HAE2NI <hae0_02ni@ewhain.net>
2025-05-19 23:35:33 +0900

    [ design ] Description

--- agoda_toy/src/stInfo/components/Upper/Description/Description.tsx
+++ agoda_toy/src/stInfo/components/Upper/Description/Description.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
 import HotelName from './HotelName';
 import Script from './Script';
 import Adv from './Adv';
+import styled from 'styled-components';
 
 export default function Description() {
   return (
-    <>
+    <Container>
       <HotelName />
       <Script />
       <Adv />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
