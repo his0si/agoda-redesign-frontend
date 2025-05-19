6a7b72fae04f888b162786fe20c5e62c0c786832
HAE2NI <hae0_02ni@ewhain.net>
2025-05-19 23:34:26 +0900

    [ design ] RoomSlider

--- agoda_toy/src/stInfo/components/Middle/Room/RoomSlider.tsx
+++ agoda_toy/src/stInfo/components/Middle/Room/RoomSlider.tsx
@@ -1,10 +1,61 @@
 import styled from 'styled-components';
 import RoomCard from './RoomCard';
+import RoomPic1 from '../../../assets/imgs/img_card1.png';
+import RoomPic2 from '../../../assets/imgs/img_card2.png';
+import RoomPic3 from '../../../assets/imgs/img_card3.png';
+import RoomPic4 from '../../../assets/imgs/img_card4.png';
+import RoomPic5 from '../../../assets/imgs/img_card5.png';
+
+const ROOM_LIST = [
+  {
+    image: RoomPic1,
+    title: '슈페리어 트윈',
+    description: '싱글베드 2개',
+    realprice: '￦ 310,597',
+    saleprice: '￦ 275,928',
+  },
+  {
+    image: RoomPic2,
+    title: '디럭스스 트윈',
+    description: '싱글베드 2개',
+    realprice: '￦ 335,244',
+    saleprice: '￦ 277,320',
+  },
+  {
+    image: RoomPic3,
+    title: '슈페리어 더블블',
+    description: '퀸베드 1개',
+    realprice: '￦ 395,200',
+    saleprice: '￦ 319,459',
+  },
+  {
+    image: RoomPic4,
+    title: '디럭스 킹킹',
+    description: '킹베드 1개개',
+    realprice: '￦ 410,100',
+    saleprice: '￦ 354,292',
+  },
+  {
+    image: RoomPic5,
+    title: '디럭스 트윈',
+    description: '싱글베드 2개+소파베드 1개',
+    realprice: '￦ 421,244',
+    saleprice: '￦ 378,527',
+  },
+];
 
 export default function RoomSlider() {
   return (
     <Container>
-      <RoomCard />
+      {ROOM_LIST.map(({ image, title, description, realprice, saleprice }) => (
+        <RoomCard
+          image={image}
+          title={title}
+          description={description}
+          realprice={realprice}
+          saleprice={saleprice}
+        />
+      ))}
       <RoomSliderBar />
       <RoomSliderBarRectangle />
     </Container>
@@ -17,6 +68,7 @@ const Container = styled.div`
   align-items: center;
   gap: 0.875rem;
   align-self: stretch;
+  overflow-x: hidden;
 `;
 
 const RoomSliderBar = styled.div`
