94478c4e24ae024ca5de38c291e0a2a6f645c10e
HAE2NI <hae0_02ni@ewhain.net>
2025-05-19 23:33:53 +0900

    [ design ] Middle

---
+++ agoda_toy/src/stInfo/components/Middle/Middle.tsx
@@ -0,0 +1,13 @@
+import AdInfo from './AdInfo/AdInfo';
+import Amen from './Amen/Amen';
+import Room from './Room/Room';
+
+export default function Middle() {
+  return (
+    <>
+      <Room />
+      <Amen />
+      <AdInfo />
+    </>
+  );
+}
