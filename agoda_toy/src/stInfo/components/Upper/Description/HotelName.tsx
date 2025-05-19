c5886f3753d91cf6c25c9f11996914f1cdbdb3a2
HAE2NI <hae0_02ni@ewhain.net>
2025-05-19 23:35:23 +0900

    [ design ] HotelName

--- agoda_toy/src/stInfo/components/Upper/Description/HotelName.tsx
+++ agoda_toy/src/stInfo/components/Upper/Description/HotelName.tsx
@@ -1,27 +1,43 @@
 import styled from 'styled-components';
-import { ColumnCenterFlex } from '../../../../styles/common/common';
+import IconHotelBottomScore from '@stInfo/assets/svgs/icn_hotel_bottom_score.svg?react';
 
 export default function HotelName() {
   return (
     <Container>
-      <TypoHotelTopFrameMain>도쿄 프린스 호텔</TypoHotelTopFrameMain>
-      <TypoHotelTopFrameSub>Tokyo Prince Hotel</TypoHotelTopFrameSub>
-      <HotelTopAdd>
-        3-3-1 Shibakoen, Minato-Ku, 롯폰기, 도쿄/동경, 일본, 105-8560
-      </HotelTopAdd>
-      <HotelBottom>8.6</HotelBottom>
-      <HotelBottom>리뷰 4,745개개</HotelBottom>
+      <HotelTopGroup>
+        <HotelTopFrame>
+          <TypoHotelTopFrameMain>도쿄 프린스 호텔</TypoHotelTopFrameMain>
+          <TypoHotelTopFrameSub>Tokyo Prince Hotel</TypoHotelTopFrameSub>
+        </HotelTopFrame>
+        <HotelTopAdd>
+          3-3-1 Shibakoen, Minato-Ku, 롯폰기, 도쿄/동경, 일본, 105-8560
+        </HotelTopAdd>
+      </HotelTopGroup>
+      <HotelBottomGroup>
+        <HotelBottom>
+          <IconStar />
+          <HotelBottomReview>8.6</HotelBottomReview>
+        </HotelBottom>
+        <HotelBottomReview>리뷰 4,745개</HotelBottomReview>
+      </HotelBottomGroup>
     </Container>
   );
 }
 
 const Container = styled.div`
-  ${ColumnCenterFlex}
+  display: flex;
+  width: 29rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+  flex-shrink: 0;
   color: ${({ theme }) => theme.colors.black};
+  margin-bottom: 3.75rem;
 `;
 
 const TypoHotelTopFrameMain = styled.h1`
   ${({ theme }) => theme.fonts.display.md};
+  margin: 0;
 `;
 
 const TypoHotelTopFrameSub = styled.h2`
@@ -31,8 +47,44 @@ const TypoHotelTopFrameSub = styled.h2`
 
 const HotelTopAdd = styled.p`
   ${({ theme }) => theme.fonts.title.sm};
+  margin: 0;
 `;
 
-const HotelBottom = styled.p`
+const HotelTopFrame = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.8125rem;
+  width: 25.5rem;
+  height: 2.81rem;
+`;
+
+const HotelTopGroup = styled.div`
+  display: flex;
+  padding: 0rem 0.5rem;
+  flex-direction: column;
+  justify-content: stretch;
+  height: 4.31rem;
+`;
+
+const HotelBottomGroup = styled.div`
+  display: flex;
+  padding: 0rem 0.5rem;
+  align-items: center;
+  gap: 1.25rem;
+`;
+
+const HotelBottom = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const HotelBottomReview = styled.p`
+  margin: 0;
   ${({ theme }) => theme.fonts.headline.md};
+`;
+
+const IconStar = styled(IconHotelBottomScore)`
+  width: 1.115rem;
+  height: 1.0625rem;
 `;
