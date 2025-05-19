b314f6e8e2fb1c93d2d4f16967b3339bfe40865e
HAE2NI <hae0_02ni@ewhain.net>
2025-05-19 23:37:08 +0900

    [ design ] App.tsx

--- agoda_toy/src/App.tsx
+++ agoda_toy/src/App.tsx
@@ -1,29 +1,34 @@
 import reset from 'styled-reset';
 import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import theme from './styles/theme/theme';
+import StInfo from './stInfo/page/StInfo';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Global>
-          <div>project</div>
-        </Global>
-        <ThemedBox> Theme 테스트</ThemedBox>
+        <Global />
+        <StInfo />
       </ThemeProvider>
     </>
   );
 }
 
 const Global = createGlobalStyle`
+  width: 120rem;
+  p {
+    margin: 0;
+  }
+
+
     ${reset}
   `;
 
-const ThemedBox = styled.div`
-  background-color: ${({ theme }) => theme.colors.secondary};
-  color: ${({ theme }) => theme.colors.white};
-  padding: 20px;
-  ${({ theme }) => theme.fonts.display.lg}
-`;
+// const ThemedBox = styled.div`
+//   background-color: ${({ theme }) => theme.colors.secondary};
+//   color: ${({ theme }) => theme.colors.white};
+//   padding: 20px;
+//   ${({ theme }) => theme.fonts.display.lg}
+// `;
 
 export default App;
