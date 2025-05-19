db9b6568133a03a0e52606859935fa49ab830a68
HAE2NI <hae0_02ni@ewhain.net>
2025-05-19 23:31:39 +0900

    [ design ] add StInfo

---
+++ agoda_toy/src/stInfo/page/StInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Upper from '../components/Upper/Upper';
+import Middle from '@stInfo/components/Middle/Middle';
+
+export default function StInfo() {
+  return (
+    <>
+      <Upper />
+      <Middle />
+    </>
+  );
+}
