c9e0a06c1e77deb6a8af1ae8549265d9335cbbd4
HAE2NI <hae0_02ni@ewhain.net>
2025-05-19 23:36:35 +0900

    [ design ] AmenContent

--- agoda_toy/src/stInfo/components/Middle/Amen/AmenContent.tsx
+++ agoda_toy/src/stInfo/components/Middle/Amen/AmenContent.tsx
@@ -1,10 +1,46 @@
 import styled from 'styled-components';
 
+const AMEN_LIST = [
+  ['무료 와이파이', '공용 화장실', '에어컨'],
+  ['조리시설', '전자레인지', '헬스장'],
+  ['커피포트', 'TV', '수영장'],
+  ['냉장고', '편의점', '인덕션'],
+  ['슬리퍼', '루프탑', '셀프 체크인'],
+  ['어메니티이', '난방', '세탁기'],
+];
+
 export default function AmenContent() {
   return (
     <Container>
       <Frame>
-        <Text>무료 와이파이</Text>
+        {AMEN_LIST[0].map((text) => (
+          <Text>{text}</Text>
+        ))}
+      </Frame>
+      <Frame>
+        {AMEN_LIST[1].map((text) => (
+          <Text>{text}</Text>
+        ))}
+      </Frame>
+      <Frame>
+        {AMEN_LIST[2].map((text) => (
+          <Text>{text}</Text>
+        ))}
+      </Frame>
+      <Frame>
+        {AMEN_LIST[3].map((text) => (
+          <Text>{text}</Text>
+        ))}
+      </Frame>
+      <Frame>
+        {AMEN_LIST[4].map((text) => (
+          <Text>{text}</Text>
+        ))}
+      </Frame>
+      <Frame>
+        {AMEN_LIST[5].map((text) => (
+          <Text>{text}</Text>
+        ))}
       </Frame>
     </Container>
   );
