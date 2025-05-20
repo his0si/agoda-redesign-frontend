5bac8dc083d366f73a936ed1e6a3e9a5308fc293
HAE2NI <hae0_02ni@ewhain.net>
2025-05-20 19:39:20 +0900

    [ design ] MainLayout

--- agoda_toy/src/common/layout/MainLayout.tsx
+++ agoda_toy/src/common/layout/MainLayout.tsx
@@ -1,15 +1,20 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <main>
+      <LayoutWrapper>
         <Outlet />
-      </main>
+      </LayoutWrapper>
       <Footer />
     </>
   );
 }
+
+const LayoutWrapper = styled.main`
+  width: 120rem;
+`;
