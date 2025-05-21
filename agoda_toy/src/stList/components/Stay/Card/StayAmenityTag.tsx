116658088f8fb36f7395a6391412959404ab0428
otwaylee <ahyeong858@gmail.com>
2025-05-22 02:48:21 +0900

    [ design ] StayAmenityPopover

--- agoda_toy/src/stList/components/Stay/Card/StayAmenityTag.tsx
+++ agoda_toy/src/stList/components/Stay/Card/StayAmenityTag.tsx
@@ -1,40 +1,54 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import StayAmenityPopover from './StayAmenityPopover';
 
 interface StayAmenityTagProps {
   tags: string[];
 }
 
 export default function StayAmenityTag({ tags }: StayAmenityTagProps) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <>
-      <TagInfo>
-        숙소 제공 사항
+      <TagInfo
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <TagListHeader>숙소 제공 사항</TagListHeader>
         <TagList>
-          {tags.map((tag, idx) => (
+          {tags.slice(0, 3).map((tag, idx) => (
             <Tag key={idx}>{tag}</Tag>
           ))}
+          {tags.length > 3 && <Tag>+{tags.length - 3}</Tag>}
         </TagList>
+        {tags.length > 3 && isHovered && <StayAmenityPopover tags={tags} />}
       </TagInfo>
     </>
   );
 }
 
 const TagInfo = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 0.75rem;
+  width: 21.5625rem;
+  height: 3.75rem;
+`;
+const TagListHeader = styled.p`
   font-family: 'SUIT';
   font-weight: 700;
   font-size: 1rem;
   line-height: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 0.75rem;
 `;
 
-const TagList = styled.div`
+export const TagList = styled.div`
   display: flex;
   gap: 0.5rem;
 `;
 
-const Tag = styled.div`
+export const Tag = styled.div`
   border: 0.0625rem solid #000;
   border-radius: 0.498rem;
   padding: 0.25rem 0.5rem;
--- agoda_toy/src/stList/components/Stay/Card/StayCard.tsx
+++ agoda_toy/src/stList/components/Stay/Card/StayCard.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import StayReview from './StayReview';
 import StayName from './StayName';
-import StayTag from './StayAmenityTag';
+import StayAmenityTag from './StayAmenityTag';
 import StayAddress from './StayLocation';
 import StayPrice from './StayPrice';
 
@@ -41,7 +41,7 @@ export default function StayCard({
             <StayName korname={korname} star={star} engName={engName} />
             <StayReview review={review} reviewCount={reviewCount} />
           </NameReviewFrame>
-          <StayTag tags={tags} />
+          <StayAmenityTag tags={tags} />
         </LeftContent>
         <RightContent>
           <StayAddress location={location} />
