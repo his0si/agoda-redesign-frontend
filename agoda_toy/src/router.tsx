6377ec9ff6d95ce384132630945ab7a9f331a562
otwaylee <ahyeong858@gmail.com>
2025-05-21 14:28:39 +0900

    [ chore ] : stlist - update router

--- agoda_toy/src/router.tsx
+++ agoda_toy/src/router.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './common/layout/MainLayout';
 import StInfo from '@stInfo/page/StInfo';
+import StList from './stList/page/StList';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    children: [{ path: '/stinfo', element: <StInfo /> }],
+    children: [
+      { path: '/stinfo', element: <StInfo /> },
+      { path: '/stlist', element: <StList /> },
+    ],
   },
 ]);
 
