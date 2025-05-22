3af95a1a6908fa49c6e0e045c1faf403b431e308
HAE2NI <hae0_02ni@ewhain.net>
2025-05-22 17:23:38 +0900

    [ feat ] add router

--- agoda_toy/src/router.tsx
+++ agoda_toy/src/router.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './common/layout/MainLayout';
 import StInfo from '@stInfo/page/StInfo';
+import MyRev from './myrev/pages/MyRev';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    children: [{ path: '/stinfo', element: <StInfo /> }],
+    children: [
+      { path: '/stinfo', element: <StInfo /> },
+      { path: 'myrev', element: <MyRev /> },
+    ],
   },
 ]);
 
