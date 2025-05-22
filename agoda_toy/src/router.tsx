a06e35542976e9a44da4fa129ef23d7356f921f0
Hee Seo Kim <hisounni@ewhain.net>
2025-05-22 09:51:42 +0900

    .

--- agoda_toy/src/router.tsx
+++ agoda_toy/src/router.tsx
@@ -3,7 +3,7 @@ import Layout from './common/layout/MainLayout';
 import HeaderOnlyLayout from './common/layout/HeaderOnlyLayout';
 import StInfo from '@stInfo/page/StInfo';
 import StList from './stList/page/StList';
-import MainPage from './pages/MainPage';
+import MainPage from './main/pages/MainPage';
 
 const router = createBrowserRouter([
   {
