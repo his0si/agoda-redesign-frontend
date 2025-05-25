bc7cbf392f52a47482f0ce4cb079f02e559505f9
otwaylee <ahyeong858@gmail.com>
2025-05-26 02:26:43 +0900

    [ design ] UpcomingResCard

---
+++ agoda_toy/src/myres/components/Upcoming/UpcomingResInfo.tsx
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+
+interface UpcomingResInfoProps {
+  resNum: number;
+  stayName: string;
+  stayLocation: string;
+  checkinDate: string;
+  checkoutDate: string;
+}
+
+export default function UpcomingResInfo({
+  resNum,
+  stayName,
+  checkinDate,
+  checkoutDate,
+  stayLocation,
+}: UpcomingResInfoProps) {
+  return (
+    <Container>
+      <TitleWrapper>
+        <Title>곧 시작될 예약</Title>
+        <SemiTitle>{resNum}</SemiTitle>
+        <SemiTitle>{stayName}</SemiTitle>
+      </TitleWrapper>
+      <TableWrapper>
+        <InfoFrame>
+          <InfoHead>숙소</InfoHead>
+          <InfoBody>{stayLocation}</InfoBody>
+        </InfoFrame>
+        <InfoFrame>
+          <InfoHead>체크인</InfoHead>
+          <InfoBody>{checkinDate}</InfoBody>
+        </InfoFrame>
+        <InfoFrame>
+          <InfoHead>체크 아웃</InfoHead>
+          <InfoBody>{checkoutDate}</InfoBody>
+        </InfoFrame>
+      </TableWrapper>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.69rem;
+  width: 46.25rem;
+  height: 16.5rem;
+  flex-shrink: 0;
+
+  border-radius: 1.25rem;
+  border: 1px solid #acacac;
+  background: #fff;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.75rem;
+`;
+
+const Title = styled.p`
+  font: ${({ theme }) => theme.fonts.title.lg};
+`;
+
+const SemiTitle = styled.p`
+  font: ${({ theme }) => theme.fonts.title.md};
+`;
+
+const TableWrapper = styled.div`
+  display: flex;
+  gap: 12.69rem;
+`;
+
+const InfoFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.69rem;
+`;
+
+const InfoHead = styled.p`
+  font: ${({ theme }) => theme.fonts.caption.md};
+`;
+const InfoBody = styled.p`
+  font: ${({ theme }) => theme.fonts.caption.md};
+`;
---
+++ agoda_toy/src/myres/components/Upcoming/UpcomingResThumb.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+import React from 'react';
+
+interface UpcomingResThumbProps {
+  imageUrl: string;
+}
+export default function UpcomingResThumb({ imageUrl }: UpcomingResThumbProps) {
+  return (
+    <>
+      <ResImage src={imageUrl} alt="곧 다가오는 예약 호텔 이미지"></ResImage>
+    </>
+  );
+}
+
+const ResImage = styled.img`
+  width: 18.25rem;
+  height: 16.5rem;
+  object-fit: cover;
+`;
