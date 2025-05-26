ad4f01cfa13d389b9ac29c9c6d5febcd08b33e6f
HAE2NI <hae0_02ni@ewhain.net>
2025-05-26 15:07:33 +0900

    [ feat ] add modal handler Fn

--- agoda_toy/src/myrev/pages/MyRev.tsx
+++ agoda_toy/src/myrev/pages/MyRev.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import { useState } from 'react';
 import MyNavBar from '../../common/components/MyNavBar';
 import MyRevCardSlider from '../components/MyRevCardSlider';
 import styled from 'styled-components';
+import MyRevModal from '../components/myRev02Modal/MyRevModal';
+import MyRevSeeModal from '../components/myRev05Modal/MyRevSeeModal';
 
 export default function MyRev() {
+  const [writeRev, setWriteRev] = useState(false);
+  const [modifyRev, setModifyRev] = useState(false);
+
+  function handleWriteRevOpen() {
+    //후기 작성 모달 오픈
+    setWriteRev(true);
+  }
+
+  function handleModifyRevOpen() {
+    //후기 수정 모달 오픈
+    setModifyRev(true);
+  }
+
+  function handleModalClose() {
+    if (writeRev) {
+      setWriteRev(false);
+    } else if (modifyRev) {
+      setModifyRev(false);
+    }
+  }
+
+  const props = {
+    handleWriteRevOpen,
+    handleModifyRevOpen,
+  };
+
   return (
-    <Containter>
-      <MyNavBar />
-      <MyRevCardSlider />
-    </Containter>
+    <>
+      {writeRev && <MyRevModal handleModalClose={handleModalClose} />}
+      {modifyRev && <MyRevSeeModal handleModalClose={handleModalClose} />}
+
+      <Containter>
+        <MyNavBar />
+        <MyRevCardSlider {...props} />
+      </Containter>
+    </>
   );
 }
 
