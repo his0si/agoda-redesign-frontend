132bd060a19b57f1783e815810dedf29b400b92d
otwaylee <ahyeong858@gmail.com>
2025-05-27 04:33:49 +0900

    [ design ] MyResContent

---
+++ agoda_toy/src/myres/components/MyResContent.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+import UpcomingResCard from './Upcoming/UpcomingResCard';
+import MyResLists from './MyResLists';
+import NoUpcomingResCard from './Upcoming/NoUpcomingResCard';
+
+export default function MyResContent() {
+  const hasUpcoming = false; // 퍼블리싱 단계에서는 하드코딩 가능
+
+  return (
+    <Container>
+      {hasUpcoming ? <UpcomingResCard /> : <NoUpcomingResCard />}
+      <MyResLists showUpcoming={hasUpcoming} />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5.5rem;
+`;
