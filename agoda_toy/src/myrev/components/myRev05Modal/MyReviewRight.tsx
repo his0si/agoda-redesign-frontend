25f6311d6f9309494207408b8e84b31e3ab5fc30
HAE2NI <hae0_02ni@ewhain.net>
2025-05-26 15:07:06 +0900

    [ design ] MyReviewRight

---
+++ agoda_toy/src/myrev/components/myRev05Modal/MyReviewRight.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './Profile';
+import ReviewPics from './ReviewPics';
+import Review from './Review';
+
+export default function MyReviewRight() {
+  return (
+    <div>
+      <Profile />
+      <ReviewPics />
+      <Review />
+    </div>
+  );
+}
