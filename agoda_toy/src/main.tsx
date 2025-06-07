2caf95e0266ad15edb6c6c557b5e95b00e960109
otwaylee <ahyeong858@gmail.com>
2025-06-07 20:18:25 +0900

    feat: react query setting

--- agoda_toy/src/main.tsx
+++ agoda_toy/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// QueryClient 인스턴스 생성하기
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
