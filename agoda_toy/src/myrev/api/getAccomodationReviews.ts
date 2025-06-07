a828eedba3e293dd7f8f646016cdcffc587dde6a
HAE2NI <hae0_02ni@ewhain.net>
2025-06-07 22:26:49 +0900

    [ feat ] get Review api

---
+++ agoda_toy/src/myrev/api/getAccomodationReviews.ts
@@ -0,0 +1,9 @@
+//숙소 리뷰 목록 조회
+import axiosInstance from '@src/common/api/axiosInstance';
+
+export const getAccommodationReviews = async (accomodationId: number) => {
+  const response = await axiosInstance.get(
+    `/accomodations/${accomodationId}/reviews`
+  );
+  return response.data;
+};
