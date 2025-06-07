481e2f30e9e9dad82fe1222c161a35175f27f35a
otwaylee <ahyeong858@gmail.com>
2025-06-07 17:40:10 +0900

    feat: axiosInstance 생성

---
+++ agoda_toy/src/common/api/axiosInstance.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 인터셉터 예시 (선택)
+axiosInstance.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default axiosInstance;
