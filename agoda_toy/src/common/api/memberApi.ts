abfc0009bc2cea362863b4d99b4f9202551b05bc
otwaylee <ahyeong858@gmail.com>
2025-06-10 17:37:14 +0900

    [ refactor ] api 메서드 수정

--- agoda_toy/src/common/api/memberApi.ts
+++ agoda_toy/src/common/api/memberApi.ts
@@ -1,6 +1,8 @@
 import axiosInstance from './axiosInstance';
 
 export const kakaoLogin = async (code: string) => {
-  const response = await axiosInstance.post('/oauth/login', { code });
+  const response = await axiosInstance.get(`/oauth/login`, {
+    params: { code },
+  });
   return response.data;
 };
